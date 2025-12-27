66deaf7ee1a782d85cf1a8a241384963e4c9e8ab
Sleepful <josepablov@gmail.com>
2025-12-26 18:36:40 -0600

    Fixes the test run (npm run test):
    - createEffect is fixed by using 'node -C browser', in order to allow effects to execute, otherwise it stubs them out for server-side rendering
    - isServer is just a variable, so remove the function call
    - createDeferred hangs the process, so use process.exit

--- test/solid/test_runner.cljs
+++ test/solid/test_runner.cljs
@@ -3,4 +3,8 @@
             [solid.core-test]))
 
 (defn main [& args]
-  (run-tests 'solid.core-test))
+  (run-tests 'solid.core-test)
+  ; `createDeferred` will hang the thread for some odd reason,
+  ; use process.exit to finish the run.
+  ; - https://github.com/solidjs/solid/issues/2570
+  (js/process.exit 0))
